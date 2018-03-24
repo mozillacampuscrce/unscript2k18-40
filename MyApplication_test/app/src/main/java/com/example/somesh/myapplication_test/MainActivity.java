1252aa8375e81618f4a4c36c32ab292a41615e55
Susan Thomas <susan.roslyn@gmail.com>
2018-03-24 20:21:21 +0530

    ..

--- MyApplication_test/app/src/main/java/com/example/somesh/myapplication_test/MainActivity.java
+++ MyApplication_test/app/src/main/java/com/example/somesh/myapplication_test/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.somesh.myapplication_test;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.content.Intent
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
